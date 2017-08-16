a449cb22f31abd4a7d4b474b9658b6816e3e6cdd
Brianna Greene <brianna.greene.designs@gmail.com>
2017-08-16 10:32:11 -0600

    new js file

---
+++ js/bootstrap.js
@@ -0,0 +1,15 @@
+//  GET ORDER FUNCTION WORKING
+// PART 1
+// on click grab card title and grab card price
+// add each to same li in sidebar
+// PART 2
+// auto matically add up all prices to equal subtotal
+// apply tax and add up total
+// PART 3
+// on submit button click, alert either:
+// success or validation message if there are no menu items added.
+
+
+// GET CAROUSEL WORKING
+// reference bootstrap
+ 
